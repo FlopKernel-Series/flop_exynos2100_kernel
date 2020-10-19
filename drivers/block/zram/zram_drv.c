604d321cca7563f401fefa65fe5a008c3c27ad46
Peter Zijlstra (Intel) <peterz@infradead.org>
2020-10-19 12:13:53 +0200

    zram: Fix __zram_bvec_{read,write}() locking order
    Mikhail reported a lockdep spat detailing how __zram_bvec_read() and
    __zram_bvec_write() use zstrm->lock and zspage->lock in opposite order.
    
    Reported-by: Mikhail Gavrilov <mikhail.v.gavrilov@gmail.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Mikhail Gavrilov <mikhail.v.gavrilov@gmail.com>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Acked-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    (cherry picked from commit 4d46f2f17fa11d60b77ea130efd612d612db059f)
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -2850,7 +2850,7 @@ out:
 static int __zram_bvec_read(struct zram *zram, struct page *page, u32 index,
 				struct bio *bio, bool partial_io)
 {
-	int ret;
+        struct zcomp_strm *zstrm;
 	unsigned long handle;
 	unsigned int size;
 	void *src, *dst;
@@ -2859,6 +2859,7 @@ static int __zram_bvec_read(struct zram *zram, struct page *page, u32 index,
 	unsigned long blk_idx;
 	bool ppr;
 #endif
+	int ret;
 
 	zram_slot_lock(zram, index);
 	if (zram_test_flag(zram, index, ZRAM_WB)) {
@@ -2912,6 +2913,9 @@ static int __zram_bvec_read(struct zram *zram, struct page *page, u32 index,
 
 	size = zram_get_obj_size(zram, index);
 
+	if (size != PAGE_SIZE)
+		zstrm = zcomp_stream_get(zram->comp);
+
 	src = zs_map_object(zram->mem_pool, handle, ZS_MM_RO);
 	if (size == PAGE_SIZE) {
 		dst = kmap_atomic(page);
@@ -2919,8 +2923,6 @@ static int __zram_bvec_read(struct zram *zram, struct page *page, u32 index,
 		kunmap_atomic(dst);
 		ret = 0;
 	} else {
-		struct zcomp_strm *zstrm = zcomp_stream_get(zram->comp);
-
 		dst = kmap_atomic(page);
 		ret = zcomp_decompress(zstrm, src, size, dst);
 		kunmap_atomic(dst);
