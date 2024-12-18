dc0fa37a66c7d4f4bdda204507146b6546d4e987
Sergey Senozhatsky <senozhatsky@chromium.org>
2024-12-18 15:34:18 +0900

    zram: free slot memory early during write
    Patch series "zram: split page type read/write handling", v2.
    
    This is a subset of [1] series which contains only fixes and improvements
    (no new features, as ZRAM_HUGE split is still under consideration).
    
    The motivation for factoring out is that zram_write_page() gets more and
    more complex all the time, because it tries to handle too many scenarios:
    ZRAM_SAME store, ZRAM_HUGE store, compress page store with zs_malloc
    allocation slowpath and conditional recompression, etc.  Factor those out
    and make things easier to handle.
    
    Addition of cond_resched() is simply a fix, I can trigger watchdog from
    zram writeback().  And early slot free is just a reasonable thing to do.
    
    [1] https://lore.kernel.org/linux-kernel/20241119072057.3440039-1-senozhatsky@chromium.org
    
    This patch (of 7):
    
    In the current implementation entry's previously allocated memory is
    released in the very last moment, when we already have allocated a new
    memory for new data.  This, basically, temporarily increases memory usage
    for no good reason.  For example, consider the case when both old (stale)
    and new entry data are incompressible so such entry will temporarily use
    two physical pages - one for stale (old) data and one for new data.  We
    can release old memory as soon as we get a write request for entry.
    
    Link: https://lkml.kernel.org/r/20241218063513.297475-1-senozhatsky@chromium.org
    Link: https://lkml.kernel.org/r/20241218063513.297475-2-senozhatsky@chromium.org
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Cc: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    (cherry picked from commit 9352acd76c7c6f09b13d3fd1e3bc044326cc9978)

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -3350,6 +3350,11 @@ static int zram_write_page(struct zram *zram, struct page *page, u32 index)
 	unsigned long irq_flags;
 #endif
 
+	/* First, free memory allocated to this slot (if any) */
+	zram_slot_lock(zram, index);
+	zram_free_page(zram, index);
+	zram_slot_unlock(zram, index);
+
 	mem = kmap_local_page(page);
 	if (page_same_filled(mem, &element)) {
 		kunmap_local(mem);
@@ -3447,13 +3452,7 @@ compress_again:
 	zs_unmap_object(zram->mem_pool, handle);
 	atomic64_add(comp_len, &zram->stats.compr_data_size);
 out:
-	/*
-	 * Free memory associated with this sector
-	 * before overwriting unused sectors.
-	 */
 	zram_slot_lock(zram, index);
-	zram_free_page(zram, index);
-
 	if (comp_len == PAGE_SIZE) {
 		zram_set_flag(zram, index, ZRAM_HUGE);
 		atomic64_inc(&zram->stats.huge_pages);
