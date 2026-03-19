be1f63dddc8c33627bbc3fb522760430c6b1f8c0
Flopster101 <nahuelgomez329@gmail.com>
2026-03-19 00:56:49 -0300

    kernel: Fix guards on KSU hooks and add missing ones
    Strictly use CONFIG_KSU_MANUAL_HOOK.
    
    Some duplicate susfs inline hooks were replaced too, as we will remove them anyway.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -385,7 +385,6 @@ static void input_handle_event(struct input_dev *dev,
 			       unsigned int type, unsigned int code, int value)
 {
 	int disposition = input_get_disposition(dev, type, code, &value);
-
 #ifdef CONFIG_KSU_MANUAL_HOOK
 	if (unlikely(ksu_input_hook))
 		ksu_handle_input_handle_event(&type, &code, &value);
