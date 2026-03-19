ae6b0026888c3eb6ad22ed36815d6c82a9bde252
Flopster101 <nahuelgomez329@gmail.com>
2026-03-19 01:27:35 -0300

    fs: Update KernelSU manual hooks (v1.9)
    * https://github.com/backslashxx/KernelSU/issues/5
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -558,10 +558,6 @@ void devpts_kill_index(struct pts_fs_info *fsi, int idx)
  *
  * The created inode is returned. Remove it from /dev/pts/ by devpts_pty_kill.
  */
-#ifdef CONFIG_KSU_SUSFS
-extern int ksu_handle_devpts(struct inode*);
-#endif
-
 struct dentry *devpts_pty_new(struct pts_fs_info *fsi, int index, void *priv)
 {
 	struct dentry *dentry;
@@ -607,10 +603,6 @@ struct dentry *devpts_pty_new(struct pts_fs_info *fsi, int index, void *priv)
  */
 void *devpts_get_priv(struct dentry *dentry)
 {
-#ifdef CONFIG_KSU_MANUAL_HOOK
-	ksu_handle_devpts(dentry->d_inode);
-#endif
-
 	if (dentry->d_sb->s_magic != DEVPTS_SUPER_MAGIC)
 		return NULL;
 	return dentry->d_fsdata;
