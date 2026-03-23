90a2678042da77f9551a13fe0a3e784d1b731870
Flopster101 <nahuelgomez329@gmail.com>
2026-03-23 16:16:52 -0300

    tools: Import from G990EXXSMHZB5
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- tools/objtool/check.c
+++ tools/objtool/check.c
@@ -147,6 +147,7 @@ static bool __dead_end_function(struct objtool_file *file, struct symbol *func,
 		"rewind_stack_and_make_dead",
 		"kunit_try_catch_throw",
 		"cpu_bringup_and_idle",
+		"kunit_try_catch_throw",
 	};
 
 	if (!func)
