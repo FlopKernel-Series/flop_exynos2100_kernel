f16f1e49b2e447dde1f2eebc565b0530215e3e97
Flopster101 <nahuelgomez329@gmail.com>
2026-03-23 23:52:50 -0300

    treewide: Nuke remaining FIVE hooks and drivers
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- include/linux/proca.h
+++ include/linux/proca.h
@@ -21,7 +21,7 @@
 #include <linux/sched.h>
 #include <linux/file.h>
 
-#if defined(CONFIG_FIVE_PA_FEATURE) || defined(CONFIG_PROCA)
+#if defined(CONFIG_PROCA)
 int proca_fcntl_setxattr(struct file *file, void __user *lv_xattr);
 #else
 static inline int proca_fcntl_setxattr(struct file *file, void __user *lv_xattr)
