98d3311b470ebb33994cc2bcde214941dd742dac
Flopster101 <nahuelgomez329@gmail.com>
2026-04-18 14:14:18 -0300

    FLOPPY: v1.0!
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- build/ckbuild.sh
+++ build/ckbuild.sh
@@ -59,7 +59,7 @@ OUT_DTBIMAGE="$TMPDIR/dtb.img"
 MKBOOTIMG="$(pwd)/build/mkbootimg/mkbootimg.py"
 MKDTBOIMG="$(pwd)/build/dtb/mkdtboimg.py"
 
-FK_VER="v1.0a"
+FK_VER="v1.0"
 USE_CCACHE="${USE_CCACHE:-1}"
 DO_TAR="${DO_TAR:-1}"
 DO_ZIP="${DO_ZIP:-1}"
