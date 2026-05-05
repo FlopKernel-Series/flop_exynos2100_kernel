15dc00fa5867a4e55dce669a52b675b9f5ddd58a
Flopster101 <nahuelgomez329@gmail.com>
2026-05-05 01:58:21 -0300

    kernel: Bye SAMSUNG_FREECESS!
    I don't need this, it's just bloat and extra overhead. It's just used by
    OneUI to assist in freezing apps apparently, but, like, just do it the way
    every other ROM does it, dammit.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -26,10 +26,6 @@
 #include "binder_alloc.h"
 #include "binder_trace.h"
 
-#ifdef CONFIG_SAMSUNG_FREECESS
-#include <linux/freecess.h>
-#endif
-
 struct list_lru binder_alloc_lru;
 
 #define MAX_ALLOCATION_SIZE (1024 * 1024)
@@ -434,19 +430,6 @@ static struct binder_buffer *binder_alloc_new_buf_locked(
 
 	/* Pad 0-size buffers so they get assigned unique addresses */
 	size = max(size, sizeof(void *));
-
-#ifdef CONFIG_SAMSUNG_FREECESS
-	if (is_async && (alloc->free_async_space < 3 * size ||
-			 alloc->free_async_space < alloc->buffer_size / 4)) {
-		struct task_struct *p;
-
-		rcu_read_lock();
-		p = find_task_by_vpid(alloc->pid);
-		rcu_read_unlock();
-		if (p && (thread_group_is_frozen(p) || p->jobctl & JOBCTL_TRAP_FREEZE))
-			binder_report(p, -1, "free_buffer_full", is_async);
-	}
-#endif
 
 	if (is_async && alloc->free_async_space < size) {
 		binder_alloc_debug(BINDER_DEBUG_BUFFER_ALLOC,
--- include/uapi/linux/netlink.h
+++ include/uapi/linux/netlink.h
@@ -29,14 +29,9 @@
 #define NETLINK_RDMA		20
 #define NETLINK_CRYPTO		21	/* Crypto layer */
 #define NETLINK_SMC		22	/* SMC monitoring */
-
 #define NETLINK_INET_DIAG	NETLINK_SOCK_DIAG
 
-/*#ifdef CONFIG_SAMSUNG_FREECESS*/
-#define NETLINK_KFREECESS       27
-/*#endif*/
-
-#define MAX_LINKS 32		
+#define MAX_LINKS 32
 
 struct sockaddr_nl {
 	__kernel_sa_family_t	nl_family;	/* AF_NETLINK	*/
