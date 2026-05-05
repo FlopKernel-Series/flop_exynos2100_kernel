99d26317742340e226c6216356c0902045be5d22
Flopster101 <nahuelgomez329@gmail.com>
2026-05-04 21:26:50 -0300

    net: wireless/realtek: rtl8xxxu-a5a5aa555oo: Fix build on 5.4
    ../drivers/net/wireless/realtek/rtl8xxxu-a5a5aa555oo/core.c:7229:10: error: use of undeclared identifier 'IEEE80211_AMPDU_TX_START_IMMEDIATE'; did you mean 'IEEE80211_AMPDU_TX_START'?
     7229 |                 return IEEE80211_AMPDU_TX_START_IMMEDIATE;
          |                        ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
          |                        IEEE80211_AMPDU_TX_START
    ../include/net/mac80211.h:3129:2: note: 'IEEE80211_AMPDU_TX_START' declared here
     3129 |         IEEE80211_AMPDU_TX_START,
          |         ^
    1 error generated.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/net/wireless/realtek/rtl8xxxu-a5a5aa555oo/core.c
+++ drivers/net/wireless/realtek/rtl8xxxu-a5a5aa555oo/core.c
@@ -7226,7 +7226,12 @@ rtl8xxxu_ampdu_action(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 		dev_dbg(dev,
 			"Changed HT: ampdu_factor %02x, ampdu_density %02x\n",
 			ampdu_factor, ampdu_density);
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 11, 0))
 		return IEEE80211_AMPDU_TX_START_IMMEDIATE;
+#else
+		ieee80211_start_tx_ba_cb_irqsafe(vif, sta->addr, tid);
+		break;
+#endif
 	case IEEE80211_AMPDU_TX_STOP_CONT:
 	case IEEE80211_AMPDU_TX_STOP_FLUSH:
 	case IEEE80211_AMPDU_TX_STOP_FLUSH_CONT:
