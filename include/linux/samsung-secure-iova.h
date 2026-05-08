3522c4eccee5bbf06d6d983dd2aaeb3b8bdb3519
oscyre1006627 <165604309+oscyre1006627@users.noreply.github.com>
2026-05-08 20:40:01 +0900

    samsung-secure-iova: Import changes from s5e9925
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- include/linux/samsung-secure-iova.h
+++ include/linux/samsung-secure-iova.h
@@ -1,5 +1,5 @@
-/* SPDX-License-Identifier: GPL-2.0
- *
+// SPDX-License-Identifier: GPL-2.0
+/*
  * Samsung IOVA Management header
  *
  * Copyright (c) 2021 Samsung Electronics Co., Ltd.
@@ -10,10 +10,9 @@
 unsigned long secure_iova_alloc(unsigned long size, unsigned int align);
 void secure_iova_free(unsigned long addr, unsigned long size);
 #else
-static inline unsigned long secure_iova_alloc(unsigned long size, unsigned int align)
+unsigned long secure_iova_alloc(unsigned long size, unsigned int align)
 {
 	return 0;
 }
-
-static inline void secure_iova_free(unsigned long addr, unsigned long size) { }
+void secure_iova_free(unsigned long addr, unsigned long size) { }
 #endif
