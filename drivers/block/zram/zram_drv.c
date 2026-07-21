b1210e245d3c156df7bd12b491a4facdbbd00934
Flopster101 <nahuelgomez329@gmail.com>
2026-07-21 15:41:40 -0300

    zram: Fix guard for ZRAM decomp fail case
    The WARN_ON otherwise would've never executed unless LRU Writeback was
    enabled.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -2927,12 +2927,13 @@ static int __zram_bvec_read(struct zram *zram, struct page *page, u32 index,
 		zcomp_stream_put(zram->comp);
 	}
 
-#ifdef CONFIG_ZRAM_LRU_WRITEBACK
 	/* Should NEVER happen. BUG() if it does. */
-	if (WARN_ON(ret))
+	if (WARN_ON(ret)) {
+#ifdef CONFIG_ZRAM_LRU_WRITEBACK
 		handle_decomp_fail(zram->compressor, ret, index, src, size,
 				   NULL);
 #endif
+	}
 
 	zs_unmap_object(zram->mem_pool, handle);
 #ifdef CONFIG_ZRAM_LRU_WRITEBACK
