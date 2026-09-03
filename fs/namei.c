7ab6f49aa59ab49130b5faba2b1f5b30fad0a986
Flopster101 <nahuelgomez329@gmail.com>
2026-09-03 01:08:45 -0300

    Revert "fs: namei: Fix NoMount kernel panic on nomount_handle_getname()"
    This reverts commit 91eb87fce5fb5dc4eb9b55ae741d4734d4d7de77.

--- fs/namei.c
+++ fs/namei.c
@@ -223,8 +223,6 @@ getname_flags(const char __user *filename, int flags, int *empty)
 #ifdef CONFIG_NOMOUNT
 	if (!IS_ERR(result)) {
 		result = nomount_handle_getname(result);
-		if (IS_ERR(result))
-			return result;
 	}
 #endif
 	audit_getname(result);
@@ -271,8 +269,6 @@ getname_kernel(const char * filename)
 #ifdef CONFIG_NOMOUNT
 	if (!IS_ERR(result)) {
 		result = nomount_handle_getname(result);
-		if (IS_ERR(result))
-			return result;
 	}
 #endif
 	audit_getname(result);
