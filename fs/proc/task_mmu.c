d3da07bd8e778da97f8d219c79647ba4ba95ef90
Flopster101 <nahuelgomez329@gmail.com>
2026-09-16 02:51:36 -0300

    kernel: OPEN_REDIRECT: Fix memory leak and possible deadlock
    - Full bug reports have been stated here: https://github.com/sidex15/susfs4ksu-module/issues/238
    
    ## Fixes ##
    1. Make susfs_srcu_open_redirect extern.
    2. Rename susfs_open_redirect_spoof_show_map_vma to susfs_open_redirect_spoof_show_map_vma_srcu to better indicate that caller needs to hold the srcu lock before calling the function and release it after use.
    3. Change the 4th arg from char *spoofed_name to char **out_spoofed_name, since passing char * acts as local copy and leaks the kzalloced buffer, eventually triggering OOM.
    
    (cherry picked from susfs4ksu gki-android12-5.10 commit 86bb893e9a103b97d17045306f35eb239a623f2a)
    (cherry picked from commit 07693e3170226613bcb0ca2c5254d51c79d673f3)
    (adapted for 5.4 exynos2100: keeps susfs_show_map_vma_spoofer name,
     keeps lineage framework-res.apk spoof and SUS_MAP handling; drops
     orig_flow label and kfree as upstream did)

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -40,7 +40,8 @@
 extern void susfs_show_map_vma_spoofer(struct inode *inode, dev_t *out_dev, unsigned long *out_ino);
 #endif
 #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
-extern int susfs_open_redirect_spoof_show_map_vma(struct inode *inode, unsigned long *out_ino, dev_t *out_dev, char *spoofed_name);
+extern struct srcu_struct susfs_srcu_open_redirect;
+extern int susfs_open_redirect_spoof_show_map_vma_srcu(struct inode *inode, unsigned long *out_ino, dev_t *out_dev, char **out_spoofed_name);
 #endif // #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
 
 #define SEQ_PUT_DEC(str, val) \
@@ -518,19 +519,28 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
 	unsigned long start, end;
 	dev_t dev = 0;
 	const char *name = NULL;
-#ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
-	char *spoofed_redirected_name = NULL;
-#endif // #ifdef CONFIG_KSU_SUSFS_OPEN_REDIREC
-
 	if (file) {
 		struct inode *inode = file_inode(vma->vm_file);
 		struct dentry *dentry;
 #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
 		if (SUSFS_IS_INODE_OPEN_REDIRECT(inode)) {
-			if (!susfs_open_redirect_spoof_show_map_vma(inode, &ino, &dev, spoofed_redirected_name)) {
+			char *spoofed_redirected_name = NULL;
+			int srcu_idx = srcu_read_lock(&susfs_srcu_open_redirect);
+			int ret = susfs_open_redirect_spoof_show_map_vma_srcu(inode, &ino, &dev, &spoofed_redirected_name);
+
+			if (!ret) {
 				pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
-				goto orig_flow;
+				start = vma->vm_start;
+				end = VMA_PAD_START(vma);
+				show_vma_header_prefix(m, start, end, flags, pgoff, dev, ino);
+				seq_pad(m, ' ');
+				if (spoofed_redirected_name)
+					seq_puts(m, spoofed_redirected_name);
+				seq_putc(m, '\n');
+				srcu_read_unlock(&susfs_srcu_open_redirect, srcu_idx);
+				return;
 			}
+			srcu_read_unlock(&susfs_srcu_open_redirect, srcu_idx);
 		}
 #endif // #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
 
@@ -558,10 +568,6 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
 		}
 	}
 
-#ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
-orig_flow:
-#endif // #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
-
 	start = vma->vm_start;
 	end = VMA_PAD_START(vma);
 	if (show_vma_header_prefix(m, start, end, flags, pgoff, dev, ino))
@@ -571,16 +577,6 @@ orig_flow:
 	 * Print the dentry name for named mappings, and a
 	 * special [heap] marker for the heap:
 	 */
-
-#ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
-	if (spoofed_redirected_name) {
-		seq_pad(m, ' ');
-		seq_puts(m, spoofed_redirected_name);
-		seq_putc(m, '\n');
-		kfree(spoofed_redirected_name);
-		return;
-	}
-#endif // #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
 	if (file) {
 		char *buf;
 		size_t size = seq_get_buf(m, &buf);
